e5cf10d680bfbe65708b49dbab3428a6d37f8f66
wujing <wujing@relaun.com>
2019-06-23 09:33:11 +0800

    delete share_book

--- share_books/models/groups.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type groups_tb struct {
-	g_id int
-	group_name string
-	describe string
-}
--- share_books/models/labels.go
+++
@@ -1,7 +0,0 @@
-package models
-
-
-type labels_tb struct {
-	l_id int
-	label_name string
-}
